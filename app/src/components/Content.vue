3bfef2519f27ac7f60e836cd2fec74376704e0b4
Kerkko Kyyrö <kerkko.kyyro@metropolia.fi>
2021-11-23 13:22:21 +0200

    Added HTML/CSS to Navbar.vue and Content.vue

---
+++ app/src/components/Content.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <ul>
+      <li id="title">{{ ideas.title }}</li>
+      <li id="desc">{{ ideas.desc }}</li>
+      <li id="upvotes">
+        <button>&uArr;</button>
+        {{ ideas.upvotes }}
+      </li>
+      <li id="user">By {{ ideas.user }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  data() {
+    return {}
+  },
+  methods: {},
+  props: {
+    ideas: Object,
+  },
+}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid black;
+  padding: 1.5em;
+  margin: 1em;
+}
+
+#title {
+  font-weight: bold;
+  font-size: x-large;
+}
+
+#desc {
+  font-style: italic;
+}
+
+#upvotes {
+  float: right;
+  font-weight: bolder;
+  font-size: xx-large;
+}
+
+#upvotes button {
+  font-size: x-large;
+  background: none;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
+
+#user {
+  font-style: oblique;
+}
+</style>
---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li class="navbtn"><a href="#">Home</a></li>
+      <li class="navbtn" id="search">
+        <form>
+          <input type="text"/>
+          <button>Search</button>
+        </form>
+      </li>
+      <ul v-if="!loggedin">
+        <li class="navbtn"><a href="#">Login</a></li>
+        <li class="navbtn"><a href="#">Register</a></li>
+      </ul>
+      <ul v-else>
+        <li class="navbtn"><a href="#">My ideas</a></li>
+        <li class="navbtn"><a href="#">Profile</a></li>
+      </ul>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      loggedin: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.navbar {
+  background-color: lightsalmon;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+.navbar ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.navbtn {
+  padding: 1em;
+  background-color: lightseagreen;
+}
+
+.navbtn a {
+  padding: 1em;
+}
+
+.navbtn:hover {
+  background-color: lightcoral;
+}
+
+#search button {
+  font-size: x-large;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
+
+#search:hover {
+  background-color: lightseagreen;
+}
+</style>
