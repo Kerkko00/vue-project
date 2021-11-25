561a284168e4734bdde5aaae43a9ecdebcfad3d5
Kerkko Kyyrö <kerkko.kyyro@metropolia.fi>
2021-11-25 14:37:26 +0200

    Added improved Navbar.vue css

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -45,52 +45,68 @@ export default {
 a {
   color: black;
   text-decoration: none;
-}
-
-.navbar ul {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  font-weight: bold;
 }
 
 .navbtn {
-  padding: 1em;
-  border: 1px solid black;
-}
-
-.navbtn a {
-  padding: 1em;
+  padding: 1em 1.5em;
+  text-align: center;
+  border-bottom: 1px solid black;
+  /* Centers text vertically */
+  line-height: 150%;
 }
 
 .navbtn:hover {
   background-color: lightcoral;
 }
 
-.navbtn:last-child {
-  border-left: none;
-}
-
-#search {
-  padding: 0;
-  margin: auto 0;
-  border: none;
-}
-
 #search input {
   padding: 0.5em;
-  margin-right: 0.25em;
   outline: none;
   border: none;
   border-radius: 1em;
+  width: 90%;
 }
 
+/*
 #search button {
   padding: 0.5em;
   border: none;
   border-radius: 1em;
 }
+ */
 
 #search:hover {
   background: none;
+}
+
+#search input:focus {
+  border: 1px solid blue;
+}
+
+@media only screen and (min-width: 600px) {
+  .navbar ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .navbtn {
+    border: 1px solid black;
+    font-size: 20px;
+  }
+
+  .navbtn:last-child {
+    border-left: none;
+  }
+
+  #search {
+    border: none;
+    flex-grow: 2;
+  }
+
+  #search input {
+    font-size: 16px;
+  }
 }
 </style>
