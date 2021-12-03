f5b0420ee9cf78f3dc2cf1ff8dd6cc3a39372c17
Kerkko Kyyrö <kerkko.kyyro@metropolia.fi>
2021-12-03 13:55:35 +0200

    Typo

--- api/validation.js
+++ api/validation.js
@@ -1,7 +1,7 @@
 const { check } = require('express-validator');
 
 exports.signupValidation = [
-    check('username', 'Name is requied').not().isEmpty(),
+    check('username', 'Name is required').not().isEmpty(),
     check('username', 'Name given in invalid form').isAlpha().isLength({min: 1, max: 30}),
     check('password', 'Password must be less than 254 characters or 6 or more characters').isLength({ min: 6, max: 254 })
 ]
